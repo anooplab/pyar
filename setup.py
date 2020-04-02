c70ace1d4befb812d66ccdc3230f4cd543681f8f
anooplab <anoop@chem.iitkgp.ac.in>
2020-04-02 22:06:30 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 setup(
     name='PyAR',
     version='0.2',
-    packages=['pyar', 'pyar.afir', 'pyar.data_analysis', 'pyar.interface'],
-    url='',
+    packages=['pyar']
+    package_dir = {'pyar': 'pyar/afir', 'pyar':'pyar.data_analysis',
+'pyar':'pyar.interface'],
+    url='https://github.com/anooplab/pyar',
     license='GPl v3',
     author='anoop et al',
-    author_email='',
+    author_email='anoop@chem.iitkgp.ac.in',
     description='',
     requires=['numpy', 'sklearn', 'scipy', 'pandas', 'matplotlib'],
 )
